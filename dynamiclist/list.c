3cce0c5c8142bfdfa2d6842cdb62d826694f138f
Pluvas159 <maniksamuel159@gmail.com>
2021-07-23 19:39:45 +0200

    Update list.c

--- dynamiclist/list.c
+++ dynamiclist/list.c
@@ -33,53 +33,55 @@ int main(){
 				ptr[x*y+temp]=c;
 				temp++;
 			}
-		} else {zle = true; break;} 
+		} else if (c!='a'){zle = true; break;} 
 	}
 	if (!zle){
 		gptr = (int*)malloc(x*y*sizeof(int));
-	for(int i = 0; i<x*y;i++){
-		*(gptr+i)=0;
-		}
-	char v;
-	for(int i =0; i<x*y;i++){
-			v = ptr[i];
-			if (v=='*'){
-				*(gptr+i)=-1;
-				if (i%x!=1){
-					if (ptr[i-1]!='*'){*(gptr+i-1) = *(gptr+i-1)+1;}
-					if (i/x!=y-1){
-						if (ptr[i+x-1]!='*'){*(gptr+i+x-1) = *(gptr+i+x-1)+1;}}
-					if (i/x!=0){
-						if (ptr[i-x-1]!='*'){*(gptr+i-x-1) = *(gptr+i-x-1)+1;}}
-					}
-				if (i%x!=0){
-						if (ptr[i+1]!='*'){*(gptr+i+1) = *(gptr+i+1)+1;}
-						if (i/x!=y-1){
-							if (ptr[i+x+1]!='*'){*(gptr+i+x+1) = *(gptr+i+x+1)+1;}}
-						if (i/x!=0){
-							if (ptr[i-x+1]!='*'){*(gptr+i-x+1) = *(gptr+i-x+1)+1;}}
-						}
-				if (i/x!=y-1){
-				if (ptr[i+x]!='*'){*(gptr+i+x) = *(gptr+i+x)+1;}}
-				if (i/x!=0){
-				if (ptr[i-x]!='*'){*(gptr+i-x) = *(gptr+i-x)+1;}}
+		for(int i = 0; i<x*y;i++){
+			*(gptr+i)=0;
 			}
-	}
-	printf("%c", ptr[30]);
-	for(int i = 0; i<x*y;i++){
-		if(i%x==0 && i!=0){printf("\n");}
-		if (*(gptr+i)==-1){printf("*");}
-		else if (*(gptr+i)==0){printf(".");}
-		else {
-		printf("%i",*(gptr+i));}
+		char v;
+		for(int i =0; i<x*y;i++){
+				v = ptr[i];
+				if (v=='*'){
+					*(gptr+i)=-1;
+					if (i%x!=1){
+						if (ptr[i-1]!='*'){*(gptr+i-1) = *(gptr+i-1)+1;}
+						if (i/x!=y-1){
+							if (ptr[i+x-1]!='*'){*(gptr+i+x-1) = *(gptr+i+x-1)+1;}}
+						if (i/x!=0){
+							if (ptr[i-x-1]!='*'){*(gptr+i-x-1) = *(gptr+i-x-1)+1;}}
+						}
+					if (i%x!=0){
+							if (ptr[i+1]!='*'){*(gptr+i+1) = *(gptr+i+1)+1;}
+							if (i/x!=y-1){
+								if (ptr[i+x+1]!='*'){*(gptr+i+x+1) = *(gptr+i+x+1)+1;}}
+							if (i/x!=0){
+								if (ptr[i-x+1]!='*'){*(gptr+i-x+1) = *(gptr+i-x+1)+1;}}
+							}
+					if (i/x!=y-1){
+					if (ptr[i+x]!='*'){*(gptr+i+x) = *(gptr+i+x)+1;}}
+					if (i/x!=0){
+					if (ptr[i-x]!='*'){*(gptr+i-x) = *(gptr+i-x)+1;}}
+				}
+		}
+		printf("%c", ptr[30]);
+		printf("\n");
+		for(int i = 0; i<x*y; i++){
+			if(i%x==0 && i!=0){printf("\n");}
+			if (*(gptr+i)==-1){printf("*");}
+			else if (*(gptr+i)==0){printf(".");}
+			else {
+			printf("%i",*(gptr+i));}
 
+		}
+	} else {
+		printf("Zly vstup\n");
 	}
-} else {
-	printf("Zly vstup\n");
-}
 
 free(ptr);
 free(gptr);
+printf("\n");
 system("pause");
 
 
